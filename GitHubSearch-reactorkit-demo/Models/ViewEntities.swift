5e44eb273e5a56f793f822fac8aba0c00c27bc49
9oya <eido9oya@gmail.com>
2023-09-27 13:06:59 +0900

    feat: Register reactors on the global di container

---
+++ GitHubSearch-reactorkit-demo/Models/ViewEntities.swift
@@ -0,0 +1,18 @@
+//
+//  ViewEntities.swift
+//  GitHubSearch-reactorkit-demo
+//
+//  Created by 9oya on 9/27/23.
+//
+
+import Foundation
+
+struct SearchEntity {
+    let title: String
+    let placeHolder: String
+}
+
+struct DetailEntity {
+    var login: String
+    var avatarUrl: String
+}
